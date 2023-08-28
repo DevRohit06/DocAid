6c84059735bb2b6afaba11c99bb21d96dd9bd843
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-08-29 00:02:18 +0530

    seo optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,15 +32,15 @@ export default function Header() {
             </h1>
           </div>
           <div className="md:hidden hidden sm:hidden xl:flex items-center gap-8 text-[#565972] ">
-            <a href="">Home</a>
-            <a href="">Find Doctor</a>
-            <a href="">Our Services</a>
-            <a href="">How It Works</a>
-            <a href="">Contact Us</a>
+            <a href="#">Home</a>
+            <a href="#">Find Doctor</a>
+            <a href="#">Our Services</a>
+            <a href="#">How It Works</a>
+            <a href="#">Contact Us</a>
           </div>
           <a
             className="py-4 px-5 bg-[#276aff] text-white rounded-lg md:hidden hidden sm:hidden xl:block"
-            href=""
+            href="#"
           >
             Book Now
           </a>
@@ -51,12 +51,12 @@ export default function Header() {
         </nav>
         {mobile ? (
           <div className="mt-12 text-xl flex flex-col md:flex xl:hidden items-center gap-8 text-[#565972] ">
-            <a href="">Home</a>
-            <a href="">Find Doctor</a>
-            <a href="">Our Services</a>
-            <a href="">How It Works</a>
-            <a href="">Contact Us</a>
-            <a className="py-4 px-5 bg-[#276aff] text-white rounded-lg" href="">
+            <a href="#">Home</a>
+            <a href="#">Find Doctor</a>
+            <a href="#">Our Services</a>
+            <a href="#">How It Works</a>
+            <a href="#">Contact Us</a>
+            <a className="py-4 px-5 bg-[#276aff] text-white rounded-lg" href="#">
               Book Now
             </a>
           </div>
