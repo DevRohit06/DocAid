b1c3396e117c8c8caba6d399c400448f6a440a7b
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-08-28 23:44:04 +0530

    feature card bg-color matching with theme

--- src/components/featureCard.jsx
+++ src/components/featureCard.jsx
@@ -4,7 +4,7 @@ export default function FCard(props) {
    
     <div className="w-full flex  items-center h-52 bg-white px-4 rounded-xl shadow-xl">
         <div className="flex items-center gap-4 px-0 sm:px-8" >
-          <div className=" rounded-full bg-blue-400 p-4 sm:p-8 flex items-center justify-center">
+          <div className=" rounded-full bg-blue-300 p-4 sm:p-8 flex items-center justify-center">
             <img
               className="sm:w-16 w-16"
               src={props.img}
